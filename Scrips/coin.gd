14e55933162e46cfbf67cb17c4a7aeaa7be0d038
Luna <anderssc@icloud.com>
2026-09-23 21:20:04 +0200

    Fixed music and coin sound

--- Scrips/coin.gd
+++ Scrips/coin.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
+
 @onready var game_manager: Node = %GameManager
 
 func _on_body_entered(body: Node2D) -> void:
-	game_manager.add_point()
+	$CollisionShape2D.queue_free()
+	$AnimatedSprite2D.queue_free()
+	$AnimationPlayer.queue_free()
+	$CoinSound.play()
+
+func _on_coin_sound_finished() -> void:
 	queue_free()
